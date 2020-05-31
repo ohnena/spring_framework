71836e9011f77fb5b9c10e61c726760354937336
ohnena <ohnenaohbaby@gmail.com>
2020-05-31 16:08:35 +0900

    #12.2 Validation Abstraction

--- src/main/java/me/ohnena/demospring51/EventValidator.java
+++ src/main/java/me/ohnena/demospring51/EventValidator.java
@@ -13,15 +13,16 @@ public class EventValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-        //o에 대하여 검증작업을 한 결과,
-        //title이라는 필드가 empty라면 에러정보를
-        // errors를 담을 거다...
-//        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "notempty", "title is not empty?!");
-        Event event = (Event)target;
-        if (event.getTitle() == null) {
-            errors.reject("this is an error code");
-            //errors.rejectValue(...)
-        }
+        // ValidationUtils를 사용하여 처리...
+        // : target에 대하여 검증작업을 한 결과, title이라는 필드가 empty라면 에러정보를 errors를 담을 거다...
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "notempty", "title is not empty?!");
+
+        //아래와같이 직접 처리도 가능...
+//        Event event = (Event)target;
+//        if (event.getTitle() == null) {
+//            errors.reject("this is an error code");
+//            //errors.rejectValue(...)
+//        }
 
     }
 }
